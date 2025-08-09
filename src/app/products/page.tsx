60643b7a673cd5a224c2d4d92c4d1c21458148a0
Frederico Velho <fredericovcoelho@gmail.com>
2025-08-09 11:12:43 -0300

    chore: add initial, config; eslint config; add chadcn ui

---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const ProductsPage = () => {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default ProductsPage;
